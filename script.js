588d461d70c279c6a10c35f790995a10d5396d7b
ignacio <ignaciomarucco@gmail.com>
2022-04-22 21:03:40 -0300

    Functions reworked

--- script.js
+++ script.js
@@ -1,72 +1,7 @@
-//computerPlay function, generates a random choice for the computer to play with.
-
-let computerPlay = () => {
-    let randomNumber = Math.floor(Math.random()*3);
-    if (randomNumber == 0) {
-        return 'rock';
-    } else if (randomNumber == 1) {
-        return 'paper';
-    } else {
-        return 'scissors';
-    }
-}
-
-let playerScore = 0;
-let computerScore = 0;
-let draws = 0;
-
-let playRound = (choice) => {
-    let playerSelection = choice;
-    let computerSelection = computerPlay();
-    console.log(playerSelection);
-    console.log(computerSelection);
-    if (playerSelection === computerSelection) {
-        draws++ ;
-        drawsText.textContent = `Draws: ${draws}`;
-    } else if (playerSelection == 'rock') {
-        if (computerSelection == 'paper') {
-            computerScore++ ;
-            computerScoreText.textContent = `Computer Score: ${computerScore}`;
-        } else {
-            playerScore++ ;
-            playerScoreText.textContent = `Player Score: ${playerScore}`;
-        }
-    } else if (playerSelection == 'paper') {
-        if (computerSelection == 'rock') {
-            playerScore++  ;
-            playerScoreText.textContent = `Player Score: ${playerScore}`;
-        } else {
-            computerScore++ ;
-            computerScoreText.textContent = `Computer Score: ${computerScore}`; 
-        }
-    } else if (playerSelection == 'scissors'){
-        if (computerSelection == 'rock') {
-            computerScore++ ;
-            computerScoreText.textContent = `Computer Score: ${computerScore}`;
-        } else {
-            playerScore++ ;
-            playerScoreText.textContent = `Player Score: ${playerScore}`;
-        }
-    } else {
-        console.log(`Choose a valid option!`) ;
-    }
-    if (playerScore == 5 || computerScore == 5) {
-        options.style.visibility = 'hidden';
-        if (playerScore > computerScore) {
-            result.textContent = `You won!`;
-        } else {
-            result.textContent = `You lost. Try again!`;
-        }
-    }
-}
-
-// DOM section:
-
 //container buttons:
 const options = document.querySelector('.options');
 
 //buttons:
-const btnNewGame = document.querySelector('#newGame');
 const btnRock = document.querySelector('#rock');
 const btnPaper = document.querySelector('#paper');
 const btnScissors = document.querySelector('#scissors');
@@ -76,30 +11,99 @@ let playerScoreText = document.querySelector('#playerScoreText');
 let computerScoreText = document.querySelector('#computerScoreText');
 let drawsText = document.querySelector('#drawsText');
 let result = document.querySelector('.result');
+let roundResult = document.querySelector('#roundResult')
+
+//Defaults
+let playerSelection;
+let playerScore = 0;
+let computerScore = 0;
+let draws = 0;
+
+// Computer Selection Function
+const rps = ["rock", "paper", "scissors"];
+
+let computerSelectionFunction = () => {
+    return rps[Math.floor(Math.random()*rps.length)];
+}
+// Player Selection Function
+let playerSelectionFunction = (event) => {
+    playerSelection = (event.target.id);
+    playRound(playerSelection, computerSelectionFunction());
+}
+btnRock.addEventListener('click', playerSelectionFunction);
+btnPaper.addEventListener('click', playerSelectionFunction);
+btnScissors.addEventListener('click', playerSelectionFunction);
+
+// Winner Function
+
+let winnerFunction = (playerSelection, computerSelection) => {
+    if (playerSelection == computerSelection) {
+        return 0;
+    } else if (
+    (playerSelection == "rock" && computerSelection == "paper") || 
+    (playerSelection == "paper" && computerSelection == "scissors") || 
+    (playerSelection == "scissors" && computerSelection == "rock") ) {
+        return 1;
+    } else {
+        return 2;
+    }
+}
+
+// Play Round Function
+let playRound = (playerSelection, computerSelection) => {
+    let winner = winnerFunction(playerSelection, computerSelection);
 
 
-//New game function:
-let game = () => {
-    options.style.visibility = 'visible';
-    btnNewGame.style.visibility = 'hidden';
-    btnPlayAgain.style.visibility = 'visible';
-    
-    let playerChoice = (event) => playRound(event.target.id);
-    btnRock.addEventListener('click', playerChoice);
-    btnPaper.addEventListener('click', playerChoice);
-    btnScissors.addEventListener('click', playerChoice);
-    
-};
+    if (winner == 0) {
+        draws++;
+        roundResult.textContent = `It's a Tie!`;
 
+        drawsText.textContent = `Draws: ${draws}`;
+    } else if (winner == 1) {
+        computerScore++;
+        roundResult.textContent = `You lost this round! ${computerSelection} beats ${playerSelection}!`;
+
+        computerScoreText.textContent = `Computer Score: ${computerScore}`; 
+    } else {
+        playerScore++;
+        roundResult.textContent = `You won this round! ${playerSelection} beats ${computerSelection}!`;
+
+        playerScoreText.textContent = `Player Score: ${playerScore}`; 
+    }
+    bestOfFive();
+}
+
+
+// Best of five
+
+let bestOfFive = () => {
+    if (playerScore == 5 || computerScore == 5 ) {
+        if (playerScore > computerScore) {
+            result.textContent = `You won the game!`; 
+        } else if (computerScore > playerScore) {
+            result.textContent = `You lost the game!`;
+        }
+        playerScore = 0;
+        computerScore = 0;
+        draws = 0;
+        btnRock.removeEventListener('click', playerSelectionFunction);
+        btnPaper.removeEventListener('click', playerSelectionFunction);
+        btnScissors.removeEventListener('click', playerSelectionFunction);
+
+
+        document.getElementById("rock").disabled = true;
+        document.getElementById("paper").disabled = true;
+        document.getElementById("scissors").disabled = true;
+
+        btnPlayAgain.style.visibility = 'visible';
+
+    }
+}
 
 //Reload function:
 let reload = () => window.location.reload();
 
 
-btnNewGame.addEventListener('click', game);
+// btnNewGame.addEventListener('click', game);
 btnPlayAgain.addEventListener('click', reload);
-
-
-
-
 
